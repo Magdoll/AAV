580e0ced77daa7c17022ee998f38b906ff21aa9e
Eric Talevich <eric@formbio.com>
2023-12-14 21:05:21 -0800

    Generate both PDF and HTML reports from Rmd
    Fix Rmd formatting (with minimal content changes) to make the LaTeX->PDF
    rendering look good.

--- create_report.R
+++ create_report.R
@@ -25,14 +25,14 @@ rmd_dir = dirname(sub("--file=", "", commandArgs(trailingOnly=FALSE)[4]))
 rmd_path = paste0(rmd_dir, "/report.Rmd")
 message(paste("Report template location:", rmd_path, sep=" "))
 
-out_path = paste0(input_prefix, "_AAV_report.html")
+out_path = paste0(input_prefix, "_AAV_report")  # Adds .html and .pdf automatically
 out_dir = dirname(out_path)
 out_filename = basename(out_path)
 message(paste("Output location:", out_path, sep=" "))
 
 rmarkdown::render(
     rmd_path,
-    output_format="html_document",
+    output_format="all",
     output_file=out_filename,
     output_dir=out_dir,
     knit_root_dir=working_dir,
